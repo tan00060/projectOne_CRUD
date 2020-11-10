0140f96c03aee0424912e94a6e9a3d13026e0c0f
Michael Tan <tan00060@algonquinlive.com>
2020-11-09 21:26:52 -0500

    added form to save state

--- src/components/ListView.js
+++ src/components/ListView.js
@@ -1,12 +1,30 @@
 import React from 'react'
 import './ListView.css'
-function ListView() {
+function ListView( {itemList} ) {
+    
+    const editButtonHandler = () => {
+        console.log("this will be the edit button")
+    }
+
+    const deleteButtonHandler = () => {
+        console.log("this will be the delete button handler")
+    }
+
     return (
         <div className="list-view-container">
-            <li className="listview-name">test item here</li>
-            <div className="listview-buttons">
-                <button>edit</button>
-                <button>delete</button>
+
+            <div className="listview-name">{itemList && itemList.map(list =>(
+                    <li>
+                        <p>{list.framework}</p>
+                        <p>{list.url}</p>
+                        <p>{list.lead}</p>
+                    </li>
+                ))}
+
+                <div className="listview-buttons">
+                    <button onClick={editButtonHandler} >edit</button>
+                    <button onClick={deleteButtonHandler} >delete</button>
+                </div>
             </div>
         </div>
     )
