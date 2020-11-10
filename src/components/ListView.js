50c5eb9e3ac869fe69aad7cdc40d86a04fe6fd05
Michael Tan <tan00060@algonquinlive.com>
2020-11-10 16:05:07 -0500

    whoops forgot to make a new branch, created listview

--- src/components/ListView.js
+++ src/components/ListView.js
@@ -1,33 +1,21 @@
 import React from 'react'
+import ListItem from './ListItem'
 import './ListView.css'
-import NewItemView from './NewItemView'
 
 function ListView( {itemList, setItemList} ) {
     
-    const editButtonHandler = () => {
-        console.log("this will be the edit button")
-    }
-
-    const deleteButtonHandler = (ev) => {
-        let id = parseInt(ev.target.id)
-        setItemList(itemList.filter((item) => item.id !== id))
-    }
-
     return (
         <div className="list-view-container">
             {itemList && itemList.map(list =>(
-                <div key={list.id} className="listview-name">
-                      <li>
-                        <p>{list.framework}</p>
-                        <p>{list.url}</p>
-                        <p>{list.lead}</p>
-                    </li>
-                    <div className="listview-buttons">
-                        <button onClick={editButtonHandler} >edit</button>
-                        <button id={list.id} onClick={deleteButtonHandler} >delete</button>
-                    </div>
-                </div>
-            ))}
+                    <ListItem 
+                        itemList={itemList}
+                        setItemList={setItemList}
+                        framework={list.framework}
+                        url={list.url}
+                        lead={list.lead}
+                        id={list.id}
+                    />
+                ))}
         </div>
     )
 }
