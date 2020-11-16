3b9e9eed7636a858ae7782d369d6488e2e10c484
Michael Tan <tan00060@algonquinlive.com>
2020-11-15 23:13:29 -0500

    fixed css for nav bar

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -6,11 +6,11 @@ import logo from '../logo.svg'
 function AppHeader() {
     return (
         <header className="header-container">
-                <div className="header-items">
+                {/* <div className="header-items"> */}
                     <img src={logo} alt="react"/>
                     <NavLink className="newItem" to="/">Javascript Frameworks</NavLink>
                     <NavLink className="newItem" to="/new_item">+ New Item</NavLink>
-                </div>
+                {/* </div> */}
         </header>
     )
 }
